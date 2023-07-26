14efc3bbfd40796f46164ad4d4b28dc122b02de1
Josie Hann <125931027+JosieHann@users.noreply.github.com>
2023-07-25 21:34:05 -0400

    added files

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Howdy from app.js ! 
-        </h1>
-      <Weather city="paris"/>
+        <Weather />
      </header>
     </div>
+    
   );
-}
+  
 
+
+  }
 export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,144 @@
-import React from "react";
-import axios from "axios";
-export default function Weather(props) {
-    function handleResponse(response) {
-        alert(`the Weather in ${response.data.name} is ${response.data.main.temp}`)
-    }
-    let apiKey = "094780c710fa4efd669f0df8c3991927";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-    
-  
-    axios.get(apiUrl).then(handleResponse);
-    return (
-        <h2>Hello from Weather!</h2>
 
+import axios from "axios";
+
+    export default function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    hours = hours < 10 ? "0" + hours : hours;
+  
+    let dayIndex = date.getDay();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    let day = days[dayIndex];
+  
+    return `${day} ${hours}:${minutes} ${ampm}`;
+  }
+  
+  function formatday(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+  
+    return days[day];
+  }
+  
+  let dateElement = document.querySelector("#date");
+  let currentTime = new Date();
+  dateElement.innerHTML = formatDate(currentTime);
+  
+  function search(event) {
+    event.preventDefault();
+    let cityElement = document.querySelector("#city");
+    let cityInput = document.querySelector("#city-input");
+    cityElement.innerHTML = cityInput.value;
+  
+    searchCity(cityInput.value);
+  }
+  let searchform = document.querySelector("#search-form");
+  searchform.addEventListener("submit", search);
+  
+  function searchCity(city) {
+    let apiKey = "2c133oabdb09a4tc70345f314f78b4fb";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial&metric`;
+    axios.get(apiUrl).then(showTemperature);
+  }
+  searchCity("Pasadena,Maryland,Usa");
+  function showTemperature(response) {
+    console.log(response);
+    const temperature = Math.round(response.data.temperature.current);
+    const h1 = document.querySelector("#city");
+    const windSpeed = response.data.wind.speed;
+    const windUnit = "mph";
+    const windElement = document.querySelector("#wind");
+    const humidityElement = document.querySelector("#humidity");
+    const temperatureElement = document.querySelector("#temperature");
+    const description = document.querySelector("#description");
+    const icon = response.data.condition.icon;
+    const displayIcon = document.querySelector("#icon");
+    displayIcon.setAttribute(
+      "src",
+      `https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${icon}.png`
     );
-}
+  
+    h1.innerHTML = response.data.city;
+    temperatureElement.innerHTML = `${temperature}°F`;
+    windElement.innerHTML = `Wind: ${windSpeed} ${windUnit}`;
+    description.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    fahrenheitTemperature = response.data.temperature.current;
+    getForecast(response.data.coordinates);
+  }
+  let fahrenheitTemperature = null;
+  
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  }
+  
+  let fahrenheitLink = document.querySelector("#fahrenheit");
+  fahrenheitLink.addEventListener("click", convertToFahrenheit);
+  
+  function convertToCelsius(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    let celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  }
+  
+  let celsiusLink = document.querySelector("#celsius");
+  celsiusLink.addEventListener("click", convertToCelsius);
+  
+  function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "2c133oabdb09a4tc70345f314f78b4fb";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=imperial`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+  }
+  function displayForecast(response) {
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+  
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+      if (index > 0 && index < 7) {
+        forecastHTML =
+          forecastHTML +
+          `
+        <div class="col-2">
+          <div class="weather-forecast-date">${formatday(forecastDay.time)}</div>
+  
+          <img
+            src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+              forecastDay.condition.icon
+            }.png" alt="Clear"
+            id="icons"
+            width="42"
+          />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">
+              ${Math.round(forecastDay.temperature.maximum)}° </span>
+            <span class="weather-forecast-temperature-min">
+            ${Math.round(forecastDay.temperature.minimum)}° </span>
+          </div>
+        </div>
+        `;
+      }
+    });
+  
+    forecastHTML += `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  }
+  
