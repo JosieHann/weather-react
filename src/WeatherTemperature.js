2c9fba39e80005429879c42f258b7fef9fe4400a
Josie Hann <125931027+JosieHann@users.noreply.github.com>
2023-08-06 19:13:09 -0400

    added celsuis covert code

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("fahrenheit");
+  function convertToCelsuis(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  
+  if (unit === 'fahrenheit') {
+    return (
+      <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.fahrenheit)}</span>
+        <span className="unit"><a href="/" onClick={convertToCelsuis}>°F</a>{" "}|°C</span> 
+       
+      </div>
+
+    );
+  } else {
+  
   return (
-    <div className="container-temp">
     <div className="WeatherTemperature">
     <span className="temperature">{Math.round(props.fahrenheit)}</span>
-      <span className="unit">°F</span>
+      <span className="unit"><a href="/" onClick={convertToFahrenheit}>°C</a>{" "}|°F</span> 
     </div>
-</div>
+
   );
+} 
 }
