dc9dc343dccb02f7a457ae5cf2629f33c4cfe298
Josie Hann <125931027+JosieHann@users.noreply.github.com>
2023-08-12 10:30:00 -0400

    deleted images

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -3,6 +3,7 @@ import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
+
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
@@ -26,21 +27,21 @@ export default function WeatherForecast(props) {
 
   if (loaded) {
     return (
-      <div className="WeatherForecast">
-        <div className="row-weather">
-          {forecast.map(function (dailyForecast, index) {
-            if (index < 5) {
-              return (
-                <div className="col" key={index}>
-                  <WeatherForecastDay data={dailyForecast} />
-                </div>
-              );
-            } else {
-              return null;
-            }
-          })}
+    <div className="WeatherForecast">
+          <div className="row-weather">
+            {forecast.map(function (dailyForecast, index) {
+              if (index < 5) {
+                return (
+                  <div className="col" key={index}>
+                    <WeatherForecastDay data={dailyForecast} />
+                  </div>
+                );
+              } else {
+                return null;
+              }
+            })}
+          </div>
         </div>
-      </div>
     );
   } else {
     load();
